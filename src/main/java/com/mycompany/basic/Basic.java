a8980b35f6da62ed9f7708d811c9169ca50c18dd
Khairul Hasan <khairul18103199@gmail.com>
2025-03-27 23:22:35 +0600

    lesson 1

---
+++ src/main/java/com/mycompany/basic/Basic.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basic;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Deepu
+ */
+public class Basic{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write something: ");
+        String input =  sc.nextLine();
+        
+        
+        System.out.println("You wrote: " + input);
+    }
+}
